cdc485e68ca4075c1db0cb543571c2a8644a44a9
Michelle <michelle.zhao@stonybrook.edu>
2021-12-22 23:05:58 -0500

    Initial commit

---
+++ app/src/main/java/com/example/simpletodo/TaskItemAdapter.kt
@@ -0,0 +1,56 @@
+package com.example.simpletodo
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+/**
+ * A bride that tells the recyclerview how to display the data we give it
+ *     specifically, takes the listOfTasks from MainActivity.kt and renders it into recyclerview item by item
+ */
+
+class TaskItemAdapter(val listOfItems: List<String>, val longClickListener: OnLongClickListener) : RecyclerView.Adapter<TaskItemAdapter.ViewHolder>() {
+
+    interface OnLongClickListener {
+        fun onItemLongCLicked(position: Int)
+    }
+
+    // Usually involves inflating a layout from XML and returning the holder
+    // Tells the recyclerview how to layout each specific item inside the recyclerview
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val context = parent.context
+        val inflater = LayoutInflater.from(context)
+        // Inflate the custom layout
+        val contactView = inflater.inflate(android.R.layout.simple_list_item_1, parent, false)
+        // Return a new holder instance
+        return ViewHolder(contactView)
+    }
+
+    // Involves populating data into the item through holder
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val item = listOfItems.get(position)
+        holder.textView.text = item         // setting textview to whatever the item is
+    }
+
+    override fun getItemCount(): Int {
+        return listOfItems.size
+    }
+
+    // Provide a direct reference to each of the views within a data item
+    // Used to cache the views within the item layout for fast access
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        // Store references to elements in our layout view
+        val textView: TextView
+
+        init {
+            textView = itemView.findViewById(android.R.id.text1)
+
+            itemView.setOnLongClickListener {
+                longClickListener.onItemLongCLicked(adapterPosition)
+                true
+            }
+        }
+    }
+}
